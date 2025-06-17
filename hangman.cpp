479727b45ff2dbb260f071e945ef7478e132ddd1
Hayes Whitney <hayeswhitney816@gmail.com>
2025-06-17 11:19:54 -0500

    Intital commit. Currently displays a random word within a list of 100 random words.

---
+++ hangman.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+
+int main(){
+
+    string words[100]={"apple", "banana", "cherry", "dog", "cat", "elephant", "house", "car", "tree", "flower",
+    "sun", "moon", "star", "book", "pen", "table", "chair", "window", "door", "sky",
+    "cloud", "rain", "snow", "wind", "fire", "water", "earth", "mountain", "river", "lake",
+    "ocean", "forest", "desert", "island", "city", "village", "road", "bridge", "school", "library",
+    "park", "garden", "farm", "market", "shop", "kitchen", "bedroom", "clock", "lamp", "mirror",
+    "phone", "computer", "television", "radio", "guitar", "piano", "drum", "song", "dance", "movie",
+    "game", "sport", "soccer", "basketball", "tennis", "swimming", "running", "jump", "walk", "dream",
+    "love", "hope", "joy", "peace", "friend", "family", "home", "food", "bread", "cheese",
+    "fruit", "vegetable", "cake", "cookie", "coffee", "tea", "juice", "milk", "fish", "bird",
+    "tiger", "lion", "bear", "wolf", "fox", "deer", "horse", "cow", "sheep", "goat"};
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dist(0,99);
+    int random_index = dist(gen);
+
+    cout<<"\n"<<words[random_index]<<"\n\n";
+
+}
